7a5c0a5ba2de8099ade0acba51352409b91a5062
Bloark <wildson.luiz@gmail.com>
2023-06-05 19:41:01 -0300

    Ajutado erro refrente ao blocos.

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
-import bgImage from '../../img/bg.png'
+import bgImage from '../../img/bg.png';
 
 export const StyledTetrisWrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: url(${bgImage}) #000;
-    background-size: cover;
-    overflow:hidden;
-`
+  width: 100vw;
+  height: 100vh;
+  background: url(${bgImage}) #000;
+  background-size: cover;
+  overflow: hidden;
+`;
 
 export const StyledTetris = styled.div`
   display: flex;
@@ -23,5 +23,4 @@ export const StyledTetris = styled.div`
     display: block;
     padding: 0 20px;
   }
-
-`
+`;
